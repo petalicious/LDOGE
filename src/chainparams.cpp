c26139aa39b8a35651986e51701d04001abe6bb0
petalicious <83580735+petalicious@users.noreply.github.com>
2021-05-13 13:56:10 +0200

    bitFlowers address magic numbers

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -96,9 +96,9 @@ public:
         vSeeds.push_back(CDNSSeedData("labs01", "petal.nerdlabs001.com"));
 
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,90);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,8);
-        base58Prefixes[SECRET_KEY]     = std::vector<unsigned char>(1,171);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,28);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,87);
+        base58Prefixes[SECRET_KEY]     = std::vector<unsigned char>(1,28+128);
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
